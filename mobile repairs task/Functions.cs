a0c08c7975e0fe4e2c34d3e121a21a57496659ab
Filopatir-Mamdouh <96422494+Filopatir-Mamdouh@users.noreply.github.com>
2023-01-01 23:21:38 +0200

    fixed bugs

--- mobile repairs task/Functions.cs
+++ mobile repairs task/Functions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -10,9 +11,38 @@ namespace mobile_repairs_task
     class Functions
     {
         private SqlConnection con;
+        private SqlCommand cmd;
+        private DataTable dt;
+        private SqlDataAdapter sda;
+        private string constr;
+
         public Functions()
         {
+            constr = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Nour\Documents\mrsDb.mdf;Integrated Security=True;Connect Timeout=30";
+            con = new SqlConnection(constr);
+            cmd = new SqlCommand();
+            cmd.Connection = con;
+        }
 
+        public DataTable getData(string query)
+        {
+            dt = new DataTable();
+            sda = new SqlDataAdapter(query, con);
+            sda.Fill(dt);
+            return dt;
+        }
+
+        public int setData(string query)
+        {
+            int r = 0;
+            if (con.State == ConnectionState.Closed)
+            {
+                con.Open();
+            }
+            cmd.CommandText = query;
+            r = cmd.ExecuteNonQuery();
+            con.Close();
+            return r;
         }
     }
 }
