c35a93c701bb49032b8297647c6a564ad3f111ca
Filopatir-Mamdouh <96422494+Filopatir-Mamdouh@users.noreply.github.com>
2023-01-02 00:14:19 +0200

    Copypasted the function from customer and edited them with find and replace

--- mobile repairs task/Spares.Designer.cs
+++ mobile repairs task/Spares.Designer.cs
@@ -30,9 +30,9 @@
         {
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Spares));
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle7 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle8 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle9 = new System.Windows.Forms.DataGridViewCellStyle();
             this.panel1 = new System.Windows.Forms.Panel();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.guna2Elipse1 = new Guna.UI2.WinForms.Guna2Elipse(this.components);
@@ -79,26 +79,26 @@
             // 
             // sparelist
             // 
-            dataGridViewCellStyle4.BackColor = System.Drawing.Color.White;
-            this.sparelist.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle4;
-            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
-            dataGridViewCellStyle5.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            dataGridViewCellStyle5.ForeColor = System.Drawing.Color.White;
-            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.sparelist.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle5;
+            dataGridViewCellStyle7.BackColor = System.Drawing.Color.White;
+            this.sparelist.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle7;
+            dataGridViewCellStyle8.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle8.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle8.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle8.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle8.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle8.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.sparelist.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle8;
             this.sparelist.ColumnHeadersHeight = 4;
             this.sparelist.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.EnableResizing;
-            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle6.BackColor = System.Drawing.Color.White;
-            dataGridViewCellStyle6.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            dataGridViewCellStyle6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
-            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
-            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
-            dataGridViewCellStyle6.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.sparelist.DefaultCellStyle = dataGridViewCellStyle6;
+            dataGridViewCellStyle9.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle9.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle9.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle9.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            dataGridViewCellStyle9.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle9.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            dataGridViewCellStyle9.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.sparelist.DefaultCellStyle = dataGridViewCellStyle9;
             this.sparelist.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
             this.sparelist.Location = new System.Drawing.Point(397, 68);
             this.sparelist.Name = "sparelist";
@@ -205,6 +205,7 @@
             this.PartName.Name = "PartName";
             this.PartName.Size = new System.Drawing.Size(177, 29);
             this.PartName.TabIndex = 17;
+            this.PartName.TextChanged += new System.EventHandler(this.PartName_TextChanged);
             // 
             // label2
             // 
--- mobile repairs task/Spares.cs
+++ mobile repairs task/Spares.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,92 @@ namespace mobile_repairs_task
 {
     public partial class Spares : Form
     {
+        Functions con;
+        string query;
         public Spares()
         {
             InitializeComponent();
+            con = new Functions();
+            showSpares();
+        }
+
+        private void PartName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void showSpares()
+        {
+            query = "SELECT * FROM sparetb";
+            sparelist.DataSource = con.getData(query);
+        }
+        private void savebtn_Click(object sender, EventArgs e)
+        {
+            if (PartName.Text == "" || Cost.Text == "")
+            {
+                MessageBox.Show("Missing Data!!!");
+            }
+            else
+            {
+                string name = PartName.Text;
+                int cost = Convert.ToInt32(Cost.Text);
+                query = "INSERT INTO sparetb VALUES('" + name + "'," + cost + ")";
+                int r = con.setData(query);
+                MessageBox.Show("Spare Added Successfully");
+                showSpares();
+
+            }
+        }
+        int key = 0;
+        private void sparelist_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            PartName.Text = sparelist.SelectedRows[0].Cells[1].Value.ToString();
+            Cost.Text = sparelist.SelectedRows[0].Cells[2].Value.ToString();
+            if (PartName.Text == "")
+            {
+                key = 0;
+            }
+            else
+                key = Convert.ToInt32(sparelist.SelectedRows[0].Cells[0].Value.ToString());
+        }
+        private void clear()
+        {
+            PartName.Text = "";
+            Cost.Text = "";
+            key = 0;
+        }
+
+        private void updatebtn_Click(object sender, EventArgs e)
+        {
+            if (PartName.Text == "" || Cost.Text == "" || key == 0)
+            {
+                MessageBox.Show("Missing Data!!!");
+            }
+            else
+            {
+                string name = PartName.Text;
+                int cost = Convert.ToInt32(Cost.Text);
+                query = "UPDATE sparetb SET SpName = '" + name + "', SpCost = " + cost + " WHERE SpID =" + key;
+                int r = con.setData(query);
+                MessageBox.Show("Spare Updated :D");
+                clear();
+                showSpares();
+            }
+        }
+
+        private void deletebtn_Click(object sender, EventArgs e)
+        {
+            if (key == 0)
+            {
+                MessageBox.Show("Please Select a Spare Part");
+            }
+            else
+            {
+                query = "DELETE FROM sparetb WHERE SpID=" + key;
+                int r = con.setData(query);
+                MessageBox.Show("Spare Deleted!!");
+                clear();
+                showSpares();
+            }
         }
     }
 }
